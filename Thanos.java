ed6d6da73b5046495c2498ea2ca53ffc3ae05d81
Arthur Dubusse <arthur.dubusse@gmail.com>
2019-11-27 17:46:05 +0100

    Comparator

--- Thanos.java
+++ Thanos.java
@@ -1,7 +1,19 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
+
+	
+	static Comparator<Hero> comparator = new Comparator<Hero>() {
+
+	    @Override
+	    public int compare(Hero hero1, Hero hero2) {
+	        return hero1.getName().compareTo(hero2.getName());
+	    }
+	};
+	
 
     public static void main(String[] args) {
 
@@ -11,15 +23,16 @@ public class Thanos {
         heroes.add(new Hero("Captain America", 100));
         heroes.add(new Hero("Thor", 1501));
 
-        // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
+     // TODO 1: Modifier Hero pour implémenter Comparable et trier par nom (croissant)
+        
+        Collections.sort(heroes, comparator);
+            System.out.println("\nOrder by name:");
+            showList(heroes);
 
 
-        System.out.println("\nOrder by name:");
-        showList(heroes);
+     // TODO 2: Ajouter un comparateur et trier par âge (décroissant)
 
-        // TODO 2: Add a Comparator and sort by age (descending)
-
-
+        Collections.sort(heroes);
         System.out.println("\nOrder by age:");
         showList(heroes);
     }
@@ -29,4 +42,8 @@ public class Thanos {
             System.out.println(heroe.getName() + ", " + heroe.getAge());
         }
     }
+
+
+
+
 }
